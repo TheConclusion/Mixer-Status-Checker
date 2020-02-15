84a804ab5421e77e03cc166c98446498851fbd87
Trigstur <Trigstur@gmail.com>
2020-02-15 19:22:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,11 +7,29 @@ print ('')
 print (' Enter the Mixer username you wish to track ')
 print ('')   	
 var = input(' Username = ')
+print ('')  
+	
+cls = lambda: os.system('cls')
+cls()
+
+print ('')   	
+print ('-- Mixer Account Tracker --')
+print ('         Set-Up           ')  
+print ('')   		
+print (' Do you want to turn on autorun mode, this will skip the bulletin and setup. ')
+print (' You will be unable to change the username to track ')
+print ('')  
+print (' Note: please only enable if you know what you are doing. ')
+print ('')
+answer = input("Please enter y/n ")
+print ('') 
+
+if answer == "y":
+   fo = open("autorun.txt", "w")
+   fo.write("autorun is enabled ( remove this file to turn it off ) ")
 
 fo = open("config.txt", "w")
 fo.write((var))
-
 fo.close()
 
-cls = lambda: os.system('cls')
 cls()
